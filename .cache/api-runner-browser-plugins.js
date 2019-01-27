d70a280bcfa6868e2289347e12246f5e57a51dad
Ghulam Hussain <ghulamhussain23@gmail.com>
2019-01-28 00:04:33 +0500

    Updates

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('/home/glykysghk/Personnel/ghulamhussain.github.io/node_modules/gatsby-plugin-web-font-loader/gatsby-browser.js'),
-      options: {"plugins":[],"google":{"families":["Lato"]}},
-    }]
